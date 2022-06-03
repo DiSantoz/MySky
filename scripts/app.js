e8a9d7b138a99a3e3621599a411c8f9132a69cf1
David Ferreira <dsferreira06@gmail.com>
2022-06-02 21:24:12 -0400

    display weather conditon icons for city searched

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,8 @@
 const cityForm = document.querySelector("form");
 const card = document.querySelector(".card");
 const details = document.querySelector(".details");
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img");
 
 const updateUI = (data) => {
   const cityDetails = data.cityDetails;
@@ -11,6 +13,20 @@ const updateUI = (data) => {
 <div class="my-3">${weather.WeatherText}</div>
 <div class="display-4 my-4"><span>${weather.Temperature.Imperial.Value}</span><span>&deg;F</span></div>
 </div>`;
+
+  // update images and icons
+
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute("src", iconSrc);
+
+  let timeSrc = null;
+  if (weather.IsDayTime) {
+    timeSrc = "img/day.svg";
+  } else {
+    timeSrc = "img/night.svg";
+  }
+  //  sets src to correct img depending on day or night
+  time.setAttribute("src", timeSrc);
 
   // display data, remove class
   if (card.classList.contains("d-none")) {
